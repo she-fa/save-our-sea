4fd235f6f23b3fcd0c439adcf10fc684b1f2feef
she-fa <syifahah@gmail.com>
2023-06-26 19:47:06 +0200

    Adding Next Level Button

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
+
 
 // Description:
 // Score system is based on the time the player sustains in the game
@@ -10,6 +12,10 @@ public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     private int score;
+    public int scoreToWin = 10;
+    public string nextLevelName = "Level1";
+    public GameObject NextLevelButton;
+
  
     void Start()
     {
@@ -19,9 +25,22 @@ public class ScoreManager : MonoBehaviour
     
     void Update()
     {
-
+        if (score> scoreToWin)
+        {
+            // Deactivate the script on another GameObject named "OtherGameObject"
+            GameObject otherObject = GameObject.Find("GameManager");
+            otherObject.GetComponent<GameOver>().enabled = false;
+            NextLevelButton.SetActive(true);
+        }
     }
    
+    // Next Level buttons
+    public void NextLevel()
+    {     
+        // Load the next level
+        SceneManager.LoadScene(nextLevelName);
+    }
+
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
